7c384dab4bf5de360c29c03b185e411b5e05b37c
k.neyman <k.neyman@corp.mail.ru>
2018-06-07 01:24:13 +0300

    Add file as arg param

--- pipe-gui/src/main/java/Pipe.java
+++ pipe-gui/src/main/java/Pipe.java
@@ -2,38 +2,55 @@ import pipe.actions.gui.PipeApplicationModel;
 import pipe.controllers.application.PipeApplicationController;
 import pipe.views.PipeApplicationBuilder;
 import pipe.views.PipeApplicationView;
+import uk.ac.imperial.pipe.parsers.UnparsableException;
 
 import javax.swing.*;
+import java.io.File;
 import java.lang.reflect.InvocationTargetException;
 
 public final class Pipe {
 
     protected static PipeApplicationView applicationView;
 
-    private Pipe(String version) {
+    private Pipe(String version, String pathToFile) {
         PipeApplicationModel applicationModel = new PipeApplicationModel(version);
         PipeApplicationController applicationController = new PipeApplicationController(applicationModel);
         PipeApplicationBuilder builder = new PipeApplicationBuilder();
         applicationView = builder.build(applicationController, applicationModel);
-        applicationController.createEmptyPetriNet();
+        openTab(applicationController, pathToFile);
+    }
 
+    private void openTab(PipeApplicationController applicationController, String pathToFile) {
+        if (pathToFile != null) {
+            try {
+                applicationController.createNewTabFromFile(new File(pathToFile));
+            } catch (UnparsableException e) {
+                e.printStackTrace();
+            }
+        } else {
+            applicationController.createEmptyPetriNet();
+        }
     }
 
     public static void main(String[] args) {
-        Runnable runnable = pipeRunnable();
+        String path = null;
+        if (args.length > 0) {
+            path = args[0];
+        }
+        Runnable runnable = pipeRunnable(path);
         SwingUtilities.invokeLater(runnable);
     }
 
-    protected static Runnable pipeRunnable() {
+    protected static Runnable pipeRunnable(final String path) {
         return new Runnable() {
             @Override
             public void run() {
-                new Pipe("v5.0.2");
+                new Pipe("v5.0.2", path);
             }
         };
     }
 
     protected static void runPipeForTesting() throws InterruptedException, InvocationTargetException {
-        SwingUtilities.invokeAndWait(pipeRunnable());
+        SwingUtilities.invokeAndWait(pipeRunnable(null));
     }
 }
